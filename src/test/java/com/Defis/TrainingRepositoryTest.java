c69d9988d8adc109afb3b798439453ebe5e0e0a4
nyundoo <nyundoo@gmail.com>
2021-08-17 18:27:31 +0300

    view form

--- src/test/java/com/Defis/TrainingRepositoryTest.java
+++ src/test/java/com/Defis/TrainingRepositoryTest.java
@@ -1,11 +1,8 @@
 package com.Defis;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.Defis.domain.Training;
 import com.Defis.repository.TrainingRepository;
 
 public class TrainingRepositoryTest {
